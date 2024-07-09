49a3f5268eefa771dea0f7e26dd32f6d6c16d825
Ishodara Perera <151984372+Ishodara-Perera@users.noreply.github.com>
2024-07-09 16:28:17 +0530

    q6 and q7

---
+++ src/q6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class q6 {
+    public static void main(String[] args) {
+        Scanner scl = new Scanner(System.in);
+
+        double [] heightarray = new double[100];
+        double [] weightarray = new double[100];
+
+
+        System.out.println("Enter the  height: ");
+        double age = scl.nextDouble();
+
+        System.out.println("Enter the  weight : ");
+        double weight = scl.nextDouble();
+
+
+
+
+    }
+
+}
---
+++ src/q7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class q7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[100];
+
+        System.out.println("enter 100 positive integers:");
+
+           int numbers = scanner.nextInt();
+        }
+    }
